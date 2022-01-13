397775cb9385dde45b44b7413aaf8538fa8b1190
DAthKim <92314150+DAthKim@users.noreply.github.com>
2022-01-13 09:10:44 +0900

    Add files via upload

--- qgis.R
+++ qgis.R
@@ -6,6 +6,14 @@ arimaCollectionRbind <- rbind(arima_predict01,arimaPredict02,arimaPredict03,arim
 # prophet
 prophetCollection <- cbind(forecast3year01,forecast3year02,forecast3year03,forecast3year05,forecast3year06, forecast3year07, stringsAsFactors = FALSE)
 
+# adfTest
+adfCollection <- rbind(adfTest01,adfTest02,adfTest03,adfTest05,adfTest06,adfTest07, stringsAsFactors = FALSE)
+
+# accuracy
+accuracyCollection <- rbind(accuracy01,accuracy02,accuracy03,accuracy05,accuracy06,accuracy07, stringsAsFactors = FALSE)
+
 write.csv(arimaCollection,"D:/내 드라이브/arimaCollection.csv")
 write.csv(arimaCollectionRbind,"D:/내 드라이브/arimaCollectionRbind.csv")
 write.csv(prophetCollection,"D:/내 드라이브/prophetCollection.csv")
+write.csv(adfCollection,"D:/내 드라이브/adfCollection.csv")
+write.csv(accuracyCollection,"D:/내 드라이브/accuracyCollection.csv")
